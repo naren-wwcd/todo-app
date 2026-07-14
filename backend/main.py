4838165e361d9bad2a8b83fc7a22814f6dfa7168
Narendran D <narenwwcdcoc@gmail.com>
2026-07-14 18:35:16 +0530

    Prepare for deployment

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ from sqlalchemy import or_
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://your-app.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
